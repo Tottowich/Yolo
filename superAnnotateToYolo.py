7ec97c4891be79b44509a9fb7e789d23ae99d34f
Tottowich <78850225+Tottowich@users.noreply.github.com>
2022-09-02 13:19:13 +0200

    Functional converter from SuperAnnotator json to YoloV5 format.

--- superAnnotateToYolo.py
+++ superAnnotateToYolo.py
@@ -2,9 +2,11 @@ import cv2
 import json
 import yaml
 import os,sys
+import random
 import argparse
 import numpy as np
 from typing import Union, Tuple
+#import torchvision.transforms as transforms
 """
     Converts SuperAnnotate JSON to YOLO format
     This includes training and validation sets, 
@@ -15,18 +17,31 @@ from typing import Union, Tuple
         - classes file (json)
         - config file (json)
     Output directory will contain:
-        - train folder containing images (png) and labels (txt)
+        - train folder containing images (png) and labels (txt)        
         - val folder containing images (png) and labels (txt)
+        - test folder containing images (png) and labels (txt)
         - data.yaml file with classes and relative directories
 """
 def get_args():
     parser = argparse.ArgumentParser(description='Converts SuperAnnotate JSON to YOLO format')
     parser.add_argument('--input', type=str, default='input', help='input directory')
     parser.add_argument('--output', type=str, default='output', help='output folder')
-    return parser.parse_args()
+    parser.add_argument('--train_val_test', nargs='+', type=float, help='Ratios per train, val, test split')
+    parser.add_argument('--img_size', nargs='+', type=int, help='Image size of output image')
+    args = parser.parse_args()
+    assert len(args.train_val_test) == 3, 'Train, val, test split must be provided'
+    assert sum(args.train_val_test) == 1, 'Train, val, test split must sum to 1'
+    assert len(args.img_size) in [2,1], 'Image size must be provided'
+    args.img_size = [args.img_size[0], args.img_size[0]] if len(args.img_size) == 1 else args.img_size
+    return args
+def get_annotation_classes_paths(args):
+    annotation_file = os.path.join(args.input, 'annotations.json')
+    classes_file = os.path.join(args.input, 'classes.json')
+    return annotation_file, classes_file
 
-def get_classes(classes_file: str)->dict:
+def get_classes(classes_file: str)->Tuple[list,int]:
     """
+    Args:
         classes_file: 
             [
                 {
@@ -37,17 +52,20 @@ def get_classes(classes_file: str)->dict:
                     "opened": bool,
                 },
             ]
+    Output:
+        classes: list of class names
+        n_classes: number of classes
+
     """
-    classes = {}
     with open(classes_file) as f:
         data = json.load(f)
-        n_classes = len(data)
-        for class_ in data:
-            classes[class_['id']-1] = class_['name']
+        classes = [d['name'] for d in data]
+        n_classes = len(classes)
     return classes, n_classes
-
-def get_annotations(annotations_file: str)->dict:
+    
+def get_annotations(annotations_file: str)->Tuple[dict,list]:
     """
+    Args:
         annotations_file:
         {
             "image_name.png": {
@@ -78,10 +96,196 @@ def get_annotations(annotations_file: str)->dict:
                 }
             },
         "image_name2.png": {...},
+    Output: 
+        annotations: dict of annotations
+        image_names: list of image names
+        
     """
     annotations = {}
+    image_names = []
     with open(annotations_file) as f:
         data = json.load(f)
-        for annotation in data:
-            annotations[annotation['image']] = annotation['instances']
-    return annotations
+        for image_name in data:
+            if not image_name.endswith('.png'):
+                continue
+            annotations[image_name] = data[image_name]
+            image_names.append(image_name)
+    random.shuffle(image_names)
+    return annotations, image_names
+
+def split_image_names(image_names:list,args)->list[list]:
+    """
+        image_names: list of image names
+        Returns: list of lists of image names
+    """
+    n_images = len(image_names)
+    n_train = int(n_images*args.train_val_test[0])
+    n_val = int(n_images*args.train_val_test[1])
+    n_test = int(n_images*args.train_val_test[2])
+    train_names = image_names[:n_train]
+    val_names = image_names[n_train:n_train+n_val]
+    test_names = image_names[n_train+n_val:]
+    return [train_names, val_names, test_names], [n_train, n_val, n_test]
+
+def get_image_path(image_name:str, input_dir:str)->str:
+    return os.path.join(input_dir, 'images', image_name)
+
+def get_image(image_path:str)->np.ndarray:
+    return cv2.imread(image_path)
+
+def get_image_size(image:np.ndarray)->Tuple[int,int]:
+    return image.shape[:2]
+
+def get_image_labels(image_name:str, annotations:dict)->str:
+    return annotations[image_name]['instances']
+def xyxy2xywh(xyxy:list)->list:
+    """
+        xyxy: list of [x1,y1,x2,y2]
+        Returns: list of [x,y,w,h]
+    """
+    x1,y1,x2,y2 = xyxy
+    w = x2-x1
+    h = y2-y1
+    x = x1+w/2
+    y = y1+h/2
+    return x,y,w,h
+def resize_image(image:np.ndarray, img_size:list)->np.ndarray:
+    return cv2.resize(image, (img_size[0], img_size[1]))
+def format_box_label(instances:list[dict], image_size:Tuple[int,int])->Tuple[str,list]:
+    """
+        instances: list of instances
+        image_size: (width, height)
+        Returns: string of bounding boxes in YOLO format
+    """
+    labels = ''
+    bboxes = []
+    for instance in instances:
+        x1 = instance['points']['x1']/image_size[1]
+        y1 = instance['points']['y1']/image_size[0]
+        x2 = instance['points']['x2']/image_size[1]
+        y2 = instance['points']['y2']/image_size[0]
+        x,y,w,h = xyxy2xywh([x1,y1,x2,y2])
+        labels += '{} {} {} {} {}\n'.format(instance['classId']-1, x, y, w, h)
+        bboxes.append([x,y,w,h])
+    return labels, bboxes
+
+def get_image_labels_yolo(image_name:str, annotations:dict,input_path:str)->str:
+    """
+        image_name: name of current image
+        annotations: dict of annotations
+        Returns: string of bounding boxes in YOLO format
+    """
+    instances = get_image_labels(image_name, annotations)
+    image_path = get_image_path(image_name, input_path)
+    image = get_image(image_path)
+    image_size = get_image_size(image)
+    return format_box_label(instances, image_size)
+
+def create_output_dir(output_dir:str,):
+    """
+    Output directory will contain:
+        - train folder containing images (png) and labels (txt)
+        - val folder containing images (png) and labels (txt)
+        - test folder containing images (png) and labels (txt)
+        - data.yaml file with classes and relative directories
+    """
+    assert not os.path.exists(output_dir), f'Output directory already exists @ {output_dir}'
+    os.mkdir(output_dir)
+    train_dir = os.path.join(output_dir, 'train')
+    val_dir = os.path.join(output_dir, 'val')
+    test_dir = os.path.join(output_dir, 'test')
+    if not os.path.exists(train_dir):
+        os.makedirs(train_dir)
+    if not os.path.exists(val_dir):
+        os.makedirs(val_dir)
+    if not os.path.exists(test_dir):
+        os.makedirs(test_dir)
+    return train_dir, val_dir, test_dir
+def add_image(image:np.ndarray, image_name:str, output_dir:str)->None:
+    """
+        image: image to be saved
+        image_name: name of image
+        output_dir: directory to save image
+    """
+    cv2.imwrite(os.path.join(output_dir, image_name), image)
+def add_label(label:str, image_name:str, output_dir:str)->None:
+    """
+        label: label to be saved
+        image_name: name of image
+        output_dir: directory to save label
+    """
+    with open(os.path.join(output_dir, image_name.split('.')[0]+'.txt'), 'w') as f:
+        f.write(label)
+
+def create_data_yaml(classes_file:str,output_dir:str)->str:
+    """
+        classes: dict of classes
+        train_dir: directory of train images
+        val_dir: directory of val images
+        test_dir: directory of test images
+        Returns: path to data.yaml file
+    """
+    classes,n_classes = get_classes(classes_file)
+    data_yaml = {
+        'path': output_dir,
+        'train': "train",
+        'val': "val",
+        'test': "test",
+        'nc': n_classes,
+        'names': classes,
+    }
+    data_yaml_path = os.path.join(output_dir, 'data.yaml')
+    with open(data_yaml_path, 'w') as f:
+        yaml.dump(data_yaml, f)
+    return data_yaml_path
+def show_image(image:np.ndarray, bboxes:list,img_size)->None:
+    """
+        image: image to be shown
+        bboxes: list of [x,y,w,h]
+        Show image with bounding box.
+    """
+    for bbox in bboxes:
+        x,y,w,h = bbox
+        x = x*img_size[1]
+        y = y*img_size[0]
+        w = w*img_size[1]
+        h = h*img_size[0]
+        image = cv2.rectangle(image, (int(x-w/2), int(y-h/2)), (int(x+w/2), int(y+h/2)), (0,255,0), 2)
+    cv2.imshow('image', image)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+def create_dataset(args):
+    """
+    Create Yolo formated dataset from SuperAnnotate format.
+    Args:
+        args: arguments
+    """
+    annotation_file, classes_file = get_annotation_classes_paths(args)
+    annotations, image_names = get_annotations(annotation_file)
+    image_names_split, n_images_split = split_image_names(image_names, args)
+    train_set = image_names_split[0]
+    val_set = image_names_split[1]
+    test_set = image_names_split[2]
+    #resizer = transforms.Resize(args.image_size)
+    train_dir, val_dir, test_dir = create_output_dir(args.output)
+    init = False
+    for set,dir in zip([train_set, val_set, test_set], [train_dir, val_dir, test_dir]):
+        for image_name in set:
+            image_path = get_image_path(image_name, args.input)
+            print(image_path)
+            image = get_image(image_path)
+            labels, bboxes = get_image_labels_yolo(image_name, annotations, args.input)
+            image = resize_image(image, args.img_size)
+            if init:
+                show_image(image, bboxes,args.img_size)
+                init = False
+            add_image(image, image_name, dir)
+            add_label(labels, image_name, dir)
+    data_yaml_path = create_data_yaml(classes_file, args.output)
+if __name__ == "__main__":
+    args = get_args()   
+    create_dataset(args)
+
+    
+    
+    
